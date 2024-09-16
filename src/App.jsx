5cab9d0084fd220835a4b387b092e6e1754b887c
naren9521 <ns952140@gmail.com>
2024-09-16 23:20:41 +0530

    modified css for chat font color and background color for toggle to work properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
-import ChatHeader from "./components/ChatHeader";
-import ChatBody from "./components/ChatBody";
-import ChatFooter from "./components/ChatFooter";
-import "./App.css"
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import ChatBox from "./components/ChatBox";
+
+import "./App.css";
 
 const App = () => {
-  const [messages, setMessages] = useState([]);
-
-  const sendMessage = (message) => {
-    if (message.trim()) {
-      const userMessage = { type: "sent", content: message };
-      setMessages((prevMessages) => [...prevMessages, userMessage]);
-
-      setTimeout(() => {
-        const botMessage = { type: "received", content: "Hey " + message };
-        setMessages((prevMessages) => [...prevMessages, botMessage]);
-      }, 1000);
-    }
+  const toggleTheme = () => {
+    document.body.classList.toggle("light-theme");
   };
 
   return (
-    <div className="chat-container">
-      <ChatHeader />
-      <ChatBody messages={messages} />
-      <ChatFooter onSendMessage={sendMessage} />
+    <div className="container">
+      <Sidebar toggleTheme={toggleTheme} />
+      <ChatBox />
     </div>
   );
 };
