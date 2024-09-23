5339273f1e5869530ff024875a41e471b7495c65
naren9521 <ns952140@gmail.com>
2024-09-23 11:33:28 +0530

    modified files

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/Chatbot/'
-=======
-  base: '/ChatBot/',
->>>>>>> f887ec24ac708413dc0574a2b733fe3ea5911dbb
-})
-
+  base: '/Chatbot/',
+});
